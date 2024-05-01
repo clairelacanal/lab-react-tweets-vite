64872e3c4371533829bd313852616d5cb1b3a962
Claire Lacanal <lacanal.claire@gmail.com>
2024-05-01 23:54:38 +0200

    lab solved

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,35 +1,21 @@
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Action from "./Action";
+
 function Tweet({user, timestamp, message}) {
   return (
     <div className="tweet">
-      <img
-        src={user.image}
-        className="profile"
-        alt="profile"
-      />
-
+      <ProfileImage image={user.image} />
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{user.name}</span>
-            <span className="handle">@{user.handle}</span>
-          </span>
-
-          <span className="timestamp">{timestamp}</span>
+          <User name={user.name} handle={user.handle}/>
+          <Timestamp time ={timestamp}/>
         </div>
-
-        <p className="message">
-          {message}
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Message message={message}/>
       </div>
-
+        <Action/>
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
